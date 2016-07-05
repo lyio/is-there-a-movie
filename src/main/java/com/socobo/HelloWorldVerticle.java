d55465dc75c277e746f5bbce51574943fed369cb
Thomas Fiedler <thfied@gmail.com>
2016-07-05 22:58:20 +0200

    Initial commit
    Includes basic hello world vertx example.

---
+++ src/main/java/com/socobo/HelloWorldVerticle.java
@@ -0,0 +1,16 @@
+package com.socobo;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+
+/**
+ * @author <a href="http://tfox.org">Tim Fox</a>
+ */
+public class HelloWorldVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        // Create an HTTP server which simply returns "Hello World!" to each request.
+        vertx.createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
+    }
+}
