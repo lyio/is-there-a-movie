0198a6154d5c7e58ca1c88403e65f0083855e9ab
Thomas Fiedler <thfied+git@gmail.com>
2016-07-11 16:27:18 +0200

    #1 Include vert.x web and enable static content serving (#2)
    * #1 Include vert.x web and enable static content serving
    
    - Add vertx web as dependency
    - Rename HelloWorldVerticle to MainVerticle
    - Add a central location for route configuration
    - Configure static file serving
    
    * #1 change MainVerticle like Tutorials, add display text to foo.html, remove imports
    
    * #1 Include vert.x web and enable static content serving
    
    - Rename package as per request

---
+++ src/main/java/com/botomo/MainVerticle.java
@@ -0,0 +1,29 @@
+package com.botomo;
+
+import com.botomo.routes.Routing;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.ext.web.Router;
+
+@SuppressWarnings("unused")
+public class MainVerticle extends AbstractVerticle {
+    
+    @Override
+	public void start(Future<Void> startFuture) throws Exception {
+		// create router
+		Router router = Router.router(vertx);
+		// set router to website
+		Routing routing = new Routing(router);
+		// create server
+		vertx
+			.createHttpServer()
+			.requestHandler(routing.register()::accept)
+			.listen(config().getInteger("http.port", 8080), result -> {
+				if (result.succeeded()) {
+					startFuture.complete();
+				} else {
+					startFuture.fail(result.cause());
+				}
+			});
+	}
+}
