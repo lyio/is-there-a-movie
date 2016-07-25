ddc3622c1744489f666c928b1b118dfb02b06444
Patrick Hebner <hebner.tec@gmail.com>
2016-07-25 12:59:18 +0200

    Only add a book if it currently doesn't exist (#34)
    #25 Prevent empty books from being accepted
    
    * Create BeanValidator
    * Error handling
    * Only add a book if the db currently doesn't contain a entity with the
    same title and author

---
+++ src/main/java/com/botomo/BeanValidator.java
@@ -0,0 +1,41 @@
+package com.botomo;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import com.botomo.models.Book;
+
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
+import io.vertx.core.Handler;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+public class BeanValidator {
+	private Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+	
+	public <T>JsonObject validate(T object){
+		
+		Set<ConstraintViolation<T>> violations = this.validator.validate(object);
+		
+		JsonObject jo = new JsonObject();
+		JsonArray ja = new JsonArray();
+		
+		for(ConstraintViolation<T> violation : violations){
+			JsonObject v = new JsonObject()
+					.put("class", violation.getLeafBean().getClass().getName())
+					.put("message", violation.getMessage())
+					.put("value", violation.getInvalidValue());
+			ja.add(v);
+		}
+		
+		jo.put("violations", ja);
+		
+		return jo;
+	}
+	
+	
+}
